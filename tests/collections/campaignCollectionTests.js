ca9d028846a13c844cb3b06e2a4d1cc15fca1825
ncphillips <ncphillips.19@gmail.com>
2016-01-30 17:38:20 -0400

    CampaignCollection now has an externally injected db dependency

--- tests/collections/campaignCollectionTests.js
+++ tests/collections/campaignCollectionTests.js
@@ -1,45 +1,41 @@
 var should = require("should");
-var CampaignCollection = require("../../lib/collections/MockCampaignCollection");
+var CampaignCollection = require("../../lib/collections/CampaignCollection");
 var Campaign = require("../../lib/models/Campaign");
 
 describe("CampaignCollection", function() {
     var campaignData = {"name": "Test Campaign"};
+    var campaignCollection = new CampaignCollection();
     describe("saving campaigns", function(){
         it("should not accept non-Campaign objects", function(){
             var badInputs = [null, undefined, "", {}, 1, 1.2, -1, [], function() {}];
 
             badInputs.forEach(function(input){
-                CampaignCollection.save.bind(null, input).should.throw();
+                campaignCollection.save.bind(null, input).should.throw();
             });
         });
 
         it("should be okay with Campaign objects", function(){
             var campaign = new Campaign(campaignData);
 
-            CampaignCollection.save.bind(null, campaign).should.not.throw();
-        });
-
-        it("should give the campaign an id", function(){
-            var campaign = new Campaign(campaignData);
-
-            campaign = CampaignCollection.save(campaign);
-
-            should.exist(campaign.getId());
+            (function() {campaignCollection.save(campaign);}).should.not.throw();
         });
     });
 
     describe("retrieving campaigns by id", function(){
         it("should return campaign with the given id", function(){
             var campaign = new Campaign(campaignData);
-            campaign = CampaignCollection.save(campaign);
 
-            var retrievedCampaign = CampaignCollection.findById(campaign.getId());
+            campaignCollection.save(campaign, function(id){
+                var retrievedCampaign = campaignCollection.findById(id);
 
-            retrievedCampaign.getId().should.equal(campaign.getId());
+                should(Campaign.prototype.isPrototypeOf(retrievedCampaign)).be.true();
+
+                should.exist(retrievedCampaign.getId());
+            });
         });
 
         it("should throw an error if there is no campaign with that id", function(){
-            (function() { CampaignCollection.findById(null);}).should.throw("No such Campaign exists");
+            (function() { campaignCollection.findById(null);}).should.throw("No such Campaign exists");
         });
     });
 
