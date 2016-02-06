23a953b2425e82628493b1b30c7a57971c1cab2a
ncphillips <ncphillips.19@gmail.com>
2016-02-06 19:09:34 -0400

    Only return monstermnaul or things you made

--- monster_manual/server/publications.js
+++ monster_manual/server/publications.js
@@ -1,6 +1,6 @@
 Meteor.publish("monsterTemplates", function(){
     if (this.userId) {
-        return MonsterTemplates.find({source: "A MM"});
+        return MonsterTemplates.find({$or: [{source: "A MM"}, {creator: this.userId}]});
     } else {
         this.ready();
     }
